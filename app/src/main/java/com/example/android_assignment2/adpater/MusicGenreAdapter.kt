5917842c54b54f70eae5625ee4f72c345c7bbc26
Jr-128 <94801671+Jr-128@users.noreply.github.com>
2021-12-26 07:21:15 -0600

    Deleted adapter file, and renamed adapter file

--- app/src/main/java/com/example/android_assignment2/adpater/MusicGenreAdapter.kt
+++ app/src/main/java/com/example/android_assignment2/adpater/MusicGenreAdapter.kt
@@ -4,10 +4,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.android_assignment2.views.ClassicFragment
 import javax.inject.Inject
 
 
-class ClassicMusicAdapter @Inject constructor(
+class MusicGenreAdapter @Inject constructor(
     val fragmentManager: FragmentManager,
     val lifecycle : Lifecycle
     ): FragmentStateAdapter(fragmentManager, lifecycle){
@@ -15,6 +16,10 @@ class ClassicMusicAdapter @Inject constructor(
     override fun getItemCount() = 3
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        if(position == 0){
+            return ClassicFragment.newInstance()
+        }
+        else
+            return ClassicFragment.newInstance()
     }
 }
--- app/src/main/java/com/example/android_assignment2/adpater/PopMusicAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.android_assignment2.adpater
-
-class PopMusicAdapter {
-}
