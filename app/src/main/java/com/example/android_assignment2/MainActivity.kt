a931706bc5c98c7e91b90ce984d6b7d3757d818d
Jr-128 <94801671+Jr-128@users.noreply.github.com>
2021-12-31 23:43:40 -0600

    Updated MainActivity.kt

--- app/src/main/java/com/example/android_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/android_assignment2/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.android_assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
+import com.example.android_assignment2.adpater.MusicGenreAdapter
 import com.example.android_assignment2.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +14,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.myContainerViewPager2.adapter = MusicGenreAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(binding.myTabLayout, binding.myContainerViewPager2)
+        { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Classic"
+                    tab.icon = getDrawable(R.drawable.ic_launcher_foreground)
+                }
+                1 -> {
+                    tab.text = "Rock"
+                    tab.icon = getDrawable(R.drawable.ic_launcher_foreground)
+                }
+                else -> {
+                    tab.text = "Pop"
+                    tab.icon = getDrawable(R.drawable.ic_launcher_foreground)
+                }
+            }
+        }.attach()
     }
 }
