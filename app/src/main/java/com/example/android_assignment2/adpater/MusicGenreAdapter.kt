49b25b158eaa5faae319f164274bb21c9eb08fb8
Jr-128 <94801671+Jr-128@users.noreply.github.com>
2021-12-31 23:39:28 -0600

    Updated MusicGenreAdapter.kt

--- app/src/main/java/com/example/android_assignment2/adpater/MusicGenreAdapter.kt
+++ app/src/main/java/com/example/android_assignment2/adpater/MusicGenreAdapter.kt
@@ -5,21 +5,29 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.android_assignment2.views.ClassicFragment
+import com.example.android_assignment2.views.PopFragment
+import com.example.android_assignment2.views.RockFragment
 import javax.inject.Inject
 
-
+/**
+ * This is the adapter to switch between fragment in the view pager
+ *
+ * @param fragmentManager We need to pass supportFragmentManager from the activity
+ * @param lifecycle We need to pass the activity lifecycle to create the fragments
+ */
 class MusicGenreAdapter @Inject constructor(
-    val fragmentManager: FragmentManager,
-    val lifecycle : Lifecycle
+    private val fragmentManager: FragmentManager,
+    private val lifecycle : Lifecycle
     ): FragmentStateAdapter(fragmentManager, lifecycle){
 
     override fun getItemCount() = 3
 
     override fun createFragment(position: Int): Fragment {
-        if(position == 0){
-            return ClassicFragment.newInstance()
+        return when(position){
+            0-> ClassicFragment.newInstance()
+            1-> RockFragment.newInstance()
+            2 -> PopFragment.newInstance()
+            else -> ClassicFragment.newInstance()
         }
-        else
-            return ClassicFragment.newInstance()
     }
 }
