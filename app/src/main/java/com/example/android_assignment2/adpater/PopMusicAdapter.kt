1b6743f20fe4aefce10afb6906a0356e36f10d27
Jr-128 <94801671+Jr-128@users.noreply.github.com>
2021-12-31 23:32:33 -0600

    Updated PopMusicAdapter.kt and RockMusicAdapter.kt, added skeleton RecyclerView code

--- app/src/main/java/com/example/android_assignment2/adpater/PopMusicAdapter.kt
+++ app/src/main/java/com/example/android_assignment2/adpater/PopMusicAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.android_assignment2.adpater
 
-class PopMusicAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import javax.inject.Inject
+
+class PopMusicAdapter @Inject constructor(
+    private val iPreviewSongClick: IPreviewSongClick
+
+) : RecyclerView.Adapter<PopMusicViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopMusicViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PopMusicViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class PopMusicViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
 }
--- app/src/main/java/com/example/android_assignment2/adpater/RockMusicAdapter.kt
+++ app/src/main/java/com/example/android_assignment2/adpater/RockMusicAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.android_assignment2.adpater
 
-class RockMusicAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import javax.inject.Inject
+
+class RockMusicAdapter @Inject constructor(
+    private val iPreviewSongClick: IPreviewSongClick
+) : RecyclerView.Adapter<RockMusicViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RockMusicViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RockMusicViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class RockMusicViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
 }
